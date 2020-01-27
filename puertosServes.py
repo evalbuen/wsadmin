14f05a646a1bf502fdf0cc5f0fca25c30eb2bebd
evalbuen <ing.efrain.valbuena@gmail.com>
2020-01-27 16:04:39 -0500

    Modificado entorno listarPuertos

--- puertosServes.py
+++ puertosServes.py
@@ -3,18 +3,19 @@ import time
 import threading
 import sys as syspython
 import re
+from pprint import pprint as pp
 
 year, month, day, hour, min = map( int, time.strftime( "%Y %m %d %H %M" ).split() )
 
 
 def guardarfile():
-    file = open( "/tmp/estado_web.txt", "w" )
+    file = open( "/tmp/listado_puertos.txt", "w" )
     for i in range( 0, len( Puertos ) ):
         file.write( Puertos[i] + '\n' )
     file.close()
 
 
-div = "**************************************************"
+div = "********************************************************************"
 tipo = "WEB_SERVER"
 
 if __name__ == "__main__":
@@ -34,13 +35,25 @@ if __name__ == "__main__":
             nname = datos.group( 2 )
             stype = AdminTask.getServerType( ['-serverName', sname, '-nodeName', nname] )
             if stype != tipo:
+                head1=("\n" + div + "\n" + ("Servidor: %s\t-\tNodo: %s" % (sname,nname)))
+                Puertos.append(head1)
+                head2=( div + "\n" + "Puerto:\t->\tNumero:\n" )
+                Puertos.append(head2)
                 nodo = ("-nodeName " + nname)
-                pts = AdminTask.listServerPorts( sname, nodo )
-                text = (div + '\n' + "Server: " + sname + " - " + nname + '\n' + "Puertos: " + pts + '\n' + div + '\n')
-                Puertos.append( text )
+                pts = AdminTask.listServerPorts( sname, nodo ).split( lineSeparator )
+                for puerto in pts:
+                    datosPuerto = re.search(
+                        "\[\[(\S.+) .+\[\[host (\S.*)\].+node (\S.+)\].+\[server (\S.+)\].+\[port (\S.+)\].+\]\]",
+                        puerto )
+                    nombrePuerto = datosPuerto.group( 1 )
+                    maquinaPuerto = datosPuerto.group( 2 )
+                    nodoPuerto = datosPuerto.group( 3 )
+                    servidorPuerto = datosPuerto.group( 4 )
+                    numeroPuerto = datosPuerto.group( 5 )
+                    text = ("%s\t->\t%s" % (nombrePuerto, numeroPuerto))
+                    Puertos.append( text )
     except:
         print( "........YOU CANT HANDLE THE TRUE", syspython.exc_info() )
-
+    cierre = ("\n" + div + "\nFinaliza el proceso de listado de puertos a las " + time.ctime() + "\n" + div + "\n")
+    Puertos.append(cierre)
 guardarfile()
-
-print( "Finaliza el proceso de subida de los WebServer a las " + time.ctime() )
